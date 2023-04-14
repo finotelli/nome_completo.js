c665888f9702800a607a9fa23839315318305626
Ana Finotelli <finotelli2004@gmail.com>
2023-04-13 21:38:55 -0300

    nome_completo.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu nome:");
+let sobrenome = prompt("Digite seu sobrenome:");
+
+alert(`Você se chama ${nome} ${sobrenome}`)
+alert(`Seu nome de catálogo é ${sobrenome.toUpperCase()}, ${nome}`)
